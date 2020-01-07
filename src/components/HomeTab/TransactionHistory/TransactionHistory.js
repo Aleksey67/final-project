c7cf077db42b787d344a835f5a4e14fac48a1703
Aleksey Baydyk <lehalevan@gmail.com>
2020-01-07 20:47:42 +0200

    hometab mobile

--- src/components/HomeTab/TransactionHistory/TransactionHistory.js
+++ src/components/HomeTab/TransactionHistory/TransactionHistory.js
@@ -42,46 +42,80 @@ const testTransactions = [
     typebalanceAfter: '-',
     updatedAt: '2020-11-25T14:14:14.100Z',
     createdAt: '2020-11-25T14:14:14.100Z',
-  }
+  },
 ];
 
 class TransactionHistory extends React.Component {
   timestampToDate(timestamp) {
-    let date = new Date(timestamp);
+    const date = new Date(timestamp);
     return date.toISOString().slice(0, 10);
   }
-  
+
   render() {
     let { transactions } = this.props;
-    
+
     if (!transactions) {
       transactions = testTransactions;
     }
-    
+
     return (
       <div className={styles.transactionHistory}>
+        <div>
+          {transactions.map(t => (
+            <div key={t.id} className={styles.transaction}>
+              <div className={styles.keyVal}>
+                <div className={styles.keyCell}>Дата</div>
+                <div className={styles.valCell}>
+                  {this.timestampToDate(t.date)}
+                </div>
+              </div>
+              <div className={styles.keyVal}>
+                <div className={styles.keyCell}>Тип</div>
+                <div className={styles.valCell}>
+                  {t.type === 'income' ? '+' : '-'}
+                </div>
+              </div>
+              <div className={styles.keyVal}>
+                <div className={styles.keyCell}>Категория</div>
+                <div className={styles.valCell}>{t.category}</div>
+              </div>
+              <div className={styles.keyVal}>
+                <div className={styles.keyCell}>Комментарий</div>
+                <div className={styles.valCell}>{t.comment}</div>
+              </div>
+              <div className={styles.keyVal}>
+                <div className={styles.keyCell}>Сумма</div>
+                <div className={styles.valCell}>{t.amount}</div>
+              </div>
+              <div className={styles.keyVal}>
+                <div className={styles.keyCell}>Баланс</div>
+                <div className={styles.valCell}>{t.balanceAfter}</div>
+              </div>
+            </div>
+          ))}
+        </div>
         <table className={styles.transactionTable}>
           <thead>
-          <tr>
-            <th>Дата</th>
-            <th>Тип</th>
-            <th>Категория</th>
-            <th>Комментарий</th>
-            <th>Сумма</th>
-            <th>Баланс</th>
-          </tr>
+            <tr>
+              <th>Дата</th>
+              <th>Тип</th>
+              <th>Категория</th>
+              <th>Комментарий</th>
+              <th>Сумма</th>
+              <th>Баланс</th>
+            </tr>
           </thead>
           <tbody>
-          {transactions.map(t => (
-            <tr key={t.id}>
-              <td>{this.timestampToDate(t.date)}</td>
-              <td>{t.type === 'income' ? '+' : '-'}</td>
-              <td>{t.category}</td>
-              <td>{t.comment}</td>
-              <td>{t.amount}</td>
-              <td>{t.balanceAfter}</td>
-            </tr>
-          ))}
+            {transactions.map(t => (
+              <tr key={t.id}>
+                <td>{this.timestampToDate(t.date)}</td>
+                <td>{t.type === 'income' ? '+' : '-'}</td>
+                <td>{t.category}</td>
+                <td>{t.comment}</td>
+                <td>{t.amount}</td>
+                <td>{t.balanceAfter}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
--- src/pages/DashboardPage/DashboardPage.js
+++ src/pages/DashboardPage/DashboardPage.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, Redirect, Link } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import HomeTab from '../../components/HomeTab/HomeTab.js';
-//import DiagramTab from '../../components/DiagramTab/DiagramTab.js';
+// import DiagramTab from '../../components/DiagramTab/DiagramTab.js';
 
 const DashboardPage = () => {
   return (
     <BrowserRouter>
       DashboardPage
-      
       <Switch>
-        <Route
-          exact
-          path={'/home'}
-          component={HomeTab}
-        />
-        {/*<Route
+        <Route exact path="/home" component={HomeTab} />
+        {/* <Route
          exact
          path={'/diagram'}
          component={DiagramTab}
-         />*/}
-        <Redirect to={'/home'} />
+         /> */}
+        <Redirect to="/home" />
       </Switch>
     </BrowserRouter>
   );
